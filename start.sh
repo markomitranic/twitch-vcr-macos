d617b30baa8ba0c9eb2598f6507b878d9f0f1534
Marko Mitranic <marko.mitranic@gmail.com>
2025-03-12 10:59:29 +0100

    Update username selection menu with new options

--- start.sh
+++ start.sh
@@ -2,17 +2,19 @@
 
 echo "Select a username:"
 echo "1) vinyljunkies"
-echo "2) dj_nitey"
+echo "2) vinylphilharmonic"
 echo "3) djfrantic"
-echo "4) vinylphilharmonic"
+echo "4) dj_nitey"
+echo "5) vinyl_transmissions"
 echo "Choose (1-6):"
 read choice
 
 case $choice in
 1) username="vinyljunkies" ;;
-2) username="dj_nitey" ;;
+2) username="vinylphilharmonic" ;;
 3) username="djfrantic" ;;
-4) username="vinylphilharmonic" ;;
+4) username="dj_nitey" ;;
+5) username="vinyl_transmissions" ;;
 *)
     echo "Invalid choice"
     exit 1

a5df35c7f2859293606dc7a596f485034096ceb3
Marko Mitranic <marko.mitranic@gmail.com>
2025-03-12 10:51:55 +0100

    Add username selection menu to start script

--- start.sh
+++ start.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-caffeinate -i -s /usr/bin/python3 twitch-recorder.py
+echo "Select a username:"
+echo "1) vinyljunkies"
+echo "2) dj_nitey"
+echo "3) djfrantic"
+echo "4) vinylphilharmonic"
+echo "Choose (1-6):"
+read choice
+
+case $choice in
+1) username="vinyljunkies" ;;
+2) username="dj_nitey" ;;
+3) username="djfrantic" ;;
+4) username="vinylphilharmonic" ;;
+*)
+    echo "Invalid choice"
+    exit 1
+    ;;
+esac
+
+echo "Starting recorder for: $username"
+caffeinate -i -s /usr/bin/python3 twitch-recorder.py --username "$username"
